91a6e2d831af968ea080cfa35133e48f2bd878c5
david <dleroys@umich.edu>
2020-12-18 04:16:45 -0500

    We measuring that depth yo

--- stereo.py
+++ stereo.py
@@ -5,6 +5,7 @@ import argparse
 import time
 import json
 import matplotlib.pyplot as plt
+from scipy.spatial import cKDTree
 
 class Stereo:
     '''
@@ -22,7 +23,7 @@ class Stereo:
         self.verbose = True if debug else verbose
         self.debug = debug
 
-    def get_depth_map(self, img1, img2, post_processing_size=1):
+    def get_depth_map(self, img1, img2, canny_thresholds=(100,200), post_processing_size=3, knn_threshold=25.0):
         '''
         Computes a depth map given stereo images
 
@@ -35,6 +36,12 @@ class Stereo:
 
         # Get features
         orb = cv2.ORB_create(nfeatures=100000, scoreType=cv2.ORB_FAST_SCORE)
+        edges1 = cv2.Canny(img1, canny_thresholds[0], canny_thresholds[1])
+        edges2 = cv2.Canny(img2, canny_thresholds[0], canny_thresholds[1])
+        edges1 = maximum_filter(edges1, size=(post_processing_size,post_processing_size))
+        edges2 = maximum_filter(edges2, size=(post_processing_size,post_processing_size))
+        edges1_pts = np.array(np.where(edges1[:,:])).T
+        edges2_pts = np.array(np.where(edges2[:,:])).T
         kp1, des1 = orb.detectAndCompute(img1,None)
         kp2, des2 = orb.detectAndCompute(img2,None)
 
@@ -49,7 +56,7 @@ class Stereo:
         if self.debug:
             match_img = cv2.drawMatches(img1,kp1,img2,kp2,matches[:50], None, flags=2)
             cv2.imshow("Matches",match_img)
-            cv2.waitKey(0)
+            #cv2.waitKey(0)
 
         # Post-processing
         post_processing_start = time.time()
@@ -69,25 +76,48 @@ class Stereo:
         disparity_map = np.zeros(img1.shape[0:2], dtype=np.uint16)
         for i, kp in enumerate(matched_kp1):
             disparity_map[kp[1],kp[0]] = disparities[i]
+        knn_start = time.time()
+        tree = cKDTree(matched_kp1[:,[1,0]])
+
+        canny_nn = tree.query(edges1_pts, distance_upper_bound=knn_threshold)[1]
+        valid_nn = canny_nn < tree.n
+        canny_disparities = disparities[canny_nn[valid_nn]]
+        for i, canny_pt in enumerate(edges1_pts[valid_nn]):
+            disparity_map[canny_pt[0],canny_pt[1]] = canny_disparities[i]
+
+        if self.verbose:
+            print("{} seconds for KNN".format(time.time() - knn_start))
+            print("{} seconds for disparity calculation".format(time.time() - disparity_start))
+        if self.debug:
+            cv2.imshow("Disparity", cv2.convertScaleAbs(disparity_map))
+            #cv2.waitKey(0)
+
+        # Filters
+        # gaussian_start = time.time()
+        # disparity_map = maximum_filter(disparity_map, size=(7,7))
+        # disparity_map = cv2.GaussianBlur(disparity_map, (11,11), 6)
+        # print(f"{time.time() - gaussian_start} seconds for filters")
+
         if self.verbose:
             print("{} seconds for disparity calculation".format(time.time() - disparity_start))
         if self.debug:
             cv2.imshow("Disparity", cv2.convertScaleAbs(disparity_map))
-            cv2.waitKey(0)
+            #cv2.waitKey(0)
 
         # Compute depth map
         depth_start = time.time()
         depth_map = np.zeros(img1.shape[0:2])
         depth_map[disparity_map > 0] = self.baseline_cm * self.focal_length_px / disparity_map[disparity_map > 0]
+
         if self.verbose:
             print("{} seconds for depth calculation".format(time.time() - depth_start))
         if self.debug:
             vis_map = np.zeros(img1.shape, dtype=np.uint8)
             print(depth_map.max())
-            vis_map[:,:,1] = (depth_map / depth_map.mean()).astype(np.uint8)
-            vis_map[vis_map != 0] = 255 - 255 * vis_map[vis_map != 0]
+            vis_map[:,:,1] = depth_map != 0
+            vis_map[vis_map != 0] *= (255 * depth_map[np.nonzero(depth_map)].min() / depth_map[depth_map != 0]).astype(np.uint8)
             cv2.imshow("Depth", vis_map)
-            cv2.waitKey(0)
+            #cv2.waitKey(0)
         print("{} seconds total".format(time.time() - start))
         return depth_map
 
