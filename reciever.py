876ba5adb072416cc9af893638abe192356f5de9
david <dleroys@umich.edu>
2020-12-18 01:10:30 -0500

    update to reciever?

--- reciever.py
+++ reciever.py
@@ -1,43 +1,21 @@
-import io
-import socket
-import struct
-from PIL import Image
-import matplotlib.pyplot as pl
+import cv2
 
-server_socket = socket.socket()
-server_socket.bind(('david-UX330UAK.local', 8000))  # ADD IP HERE
-server_socket.listen(0)
+cap = cv2.VideoCapture("http://raspberrypi.local:8080/stream/video.mjpeg")
 
-# Accept a single connection and make a file-like object out of it
-connection = server_socket.accept()[0].makefile('rb')
-try:
-    img = None
-    while True:
-        # Read the length of the image as a 32-bit unsigned int. If the
-        # length is zero, quit the loop
-        image_len = struct.unpack('<L', connection.read(struct.calcsize('<L')))[0]
-        if not image_len:
-            break
-        # Construct a stream to hold the image data and read the image
-        # data from the connection
-        image_stream = io.BytesIO()
-        image_stream.write(connection.read(image_len))
-        # Rewind the stream, open it as an image with PIL and do some
-        # processing on it
-        image_stream.seek(0)
-        image = Image.open(image_stream)
-        
-        if img is None:
-            img = pl.imshow(image)
-        else:
-            img.set_data(image)
+print(cap.isOpened())
 
-        pl.pause(0.01)
-        pl.draw()
+while(True):
+    # Capture frame-by-frame
+    ret, frame = cap.read()
 
-        print('Image is %dx%d' % image.size)
-        image.verify()
-        print('Image is verified')
-finally:
-    connection.close()
-    server_socket.close()
+    # Our operations on the frame come here
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    # Display the resulting frame
+    cv2.imshow('frame', frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# When everything done, release the capture
+cap.release()
+cv2.destroyAllWindows()
